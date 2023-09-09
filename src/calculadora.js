4acc665f7f7089ea15e3e2024d481137dd33b278
Albertoprofissionaldigital <albertoprofissionaldigital@gmail.com>
2023-09-09 17:30:08 -0300

    Adicionando novos arquivos ao repositório

---
+++ src/calculadora.js
@@ -0,0 +1,17 @@
+class Calculadora {
+
+    soma(a,b){
+        return a+b;
+    }
+    subtracao(a,b){
+        return a-b;
+    }
+    multiplicacao(a,b){
+        return a*b;
+    }
+    divisao(a,b){
+        return a/b;
+    }
+}
+
+module.exports = Calculadora;
